3bc7146d0b0dc717fc103c274f16aa3e8b0a0d26
wangnian666 <xuliang94wo@gamil.com>
2018-01-15 19:41:55 +0800

    first commit

---
+++ src/main/java/com/example/swaggerDemo/controller/DemoController.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.example.swaggerDemo.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liangxu 
+ * @create 2018年1月15日下午4:24:26
+ * @version 1.0
+ * 
+ */
+@Api("swagger测试")
+@RestController
+@RequestMapping(value="/")
+public class DemoController {
+	
+	@ApiOperation(value="方法测试",notes="根据传参返回对应的值")
+	@ResponseBody
+	@RequestMapping(value="/test",method=RequestMethod.GET)
+	public String word( @RequestParam("word") String word){
+		return word;
+	} 
+	
+	
+}
