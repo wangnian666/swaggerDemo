1112eca749fb1a5dbbd8463c8750b5d80ddf1c57
wangnian666 <xuliang94wo@gamil.com>
2018-02-10 17:30:22 +0800

    添加日志

--- src/main/java/com/example/swaggerDemo/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/swaggerDemo/service/serviceImpl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.swaggerDemo.service.serviceImpl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,7 @@ import com.example.swaggerDemo.vo.UserVo;
 @Service
 public class UserServiceImpl implements UserService {
 
+	public static Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 	@Autowired
 	private UserDao userDao;
 	@Override
@@ -34,6 +37,7 @@ public class UserServiceImpl implements UserService {
 			userVo.setName(us.getName());
 			listUserVos.add(userVo);
 		}
+		logger.info("确认正常！");
 		return listUserVos;
 	}
 }
